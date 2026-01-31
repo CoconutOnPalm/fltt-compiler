0faf41ecc5eadceae74e4b3d2cf7b3d0a3fc0da5
CoconuOnPalm <adam.kostrzewski@hotmail.com>
2026-01-31 16:16:55 +0100

    switched to argparse for arg handling

--- src/input/arg_parser.cpp
+++ src/input/arg_parser.cpp
@@ -1,32 +1,36 @@
 #include "arg_parser.hpp"
 
 #include <string>
+#include <argparse/argparse.hpp>
 
 #include "../utils/panic.hpp"
+#include "../utils/const.hpp"
 
 
 namespace fl
 {
-	template <typename... Args>
-	void argparser_error(const std::format_string<Args...> format_str, Args&&... args)
-	{
-		std::print("\033[31m[ERROR]\033[0m ");
-		std::println(std::cerr, format_str, std::forward<Args>(args)...);
-		std::println("usage: kompilator <source-code-path> <machine-code-path>");
-		std::exit(1);
-	}
 	
     ProgramArgs parse_args(const int argc, const char* const* argv)
     {
-		if (argc != 3) // {program, lang_src, mc_src}
-			argparser_error("invalid argument count; expected 2, got {}", argc - 1);
+		argparse::ArgumentParser parser("kompilator", "1.0");
+		
+		parser.add_argument<std::string>("src-file")
+			.help(".imp source file")
+			.required();
+		parser.add_argument<std::string>("asm-file")
+			.help(".mr vm-assembly file (compiler output)")
+			.required();
+		
+		try {
+			parser.parse_args(argc, argv);
+		} catch (const std::exception& e) {
+			std::println(std::cerr, "{}", e.what());
+			std::exit(1);
+		}
 
-		const std::string lang_src_path(argv[1]);
-		const std::string machine_code_path(argv[2]);
-
-        return ProgramArgs{
-			.language_path{lang_src_path},
-			.machine_code_path{machine_code_path}
+		return ProgramArgs {
+			.language_path{parser.get<std::string>("src-file")},
+			.machine_code_path{parser.get<std::string>("asm-file")},
 		};
     }
 
--- src/utils/const.hpp
+++ src/utils/const.hpp
@@ -7,6 +7,7 @@ namespace fl
 {
 	namespace config
 	{
+		constexpr std::string program_name = "kompilator";
 		
 		constexpr std::string prog_entry_name = "@__PROG_START";
 		constexpr std::string return_variable_name = "#return";
